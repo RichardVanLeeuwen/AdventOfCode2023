43762b93cffcf00930b4f7fb848309b04e4129df
Richard van Leeuwen <rbavleeuwen@gmail.com>
2023-12-07 09:17:10 +0100

    Completed day 5

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,79 @@
 import fs from "fs";
 
-let sum = 0;
-const data = fs.readFileSync("src/data.txt").toString();
-const lines = data.split("\n");
-const lines2 = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-`.split("\n");
-// part 1
-lines.forEach((line: string) => {
-    if (line === "") return;
-    const winningNumbers = line
-        .match(/\:[\s\d]+\|/g)[0]
-        .match(/\d+/g)
-        .map((str) => parseInt(str, 10));
-    const myNumbers = line
-        .match(/\|.*$/g)[0]
-        .match(/\d+/g)
-        .map((str) => parseInt(str, 10));
-    let numOfMatches = 0;
-    myNumbers.forEach((myNumber) => {
-        winningNumbers.forEach((winningNumber) => {
-            if (myNumber === winningNumber) numOfMatches++;
-        });
-    });
-    if (numOfMatches !== 0) {
-        sum += 2 ** (numOfMatches - 1);
+class Map {
+    start: number = undefined;
+    end: number = undefined;
+    diff: number = undefined;
+    constructor(line: string) {
+        const matches = line.match(/\d+/g);
+        this.start = parseInt(matches[1], 10);
+        this.end = this.start + parseInt(matches[2]);
+        this.diff = parseInt(matches[0], 10) - this.start;
     }
+    canMap = (inputNum: number): boolean => {
+        return inputNum >= this.start && inputNum < this.end;
+    };
+    map = (inputnum: number): number => {
+        return inputnum + this.diff;
+    };
+}
+
+const data = fs.readFileSync("src/data.txt").toString();
+const parts = data.split("\n\n");
+
+// part 1
+const seeds = parts[0].match(/\d+/g).map((str) => parseInt(str, 10));
+let locations: number[] = seeds;
+for (let i = 1; i <= 7; i++) {
+    locations = locations.map(createMapper(parts[1]));
+}
+let lowestLocationNumber = 0;
+locations.forEach((location) => {
+    if (location < lowestLocationNumber || lowestLocationNumber === 0)
+        lowestLocationNumber = location;
 });
-console.log("sum: ", sum);
+console.log("lowestLocationNumber: ", lowestLocationNumber);
 
 // part 2
-const cards: number[] = [];
-lines.forEach((line: string) => {
-    if (line === "") return;
-    cards.push(1);
+const startRangePairs = parts[0].match(/\d+ \d+/g).map((str) => {
+    return str.split(" ").map((str) => parseInt(str, 10));
 });
-lines.forEach((line: string) => {
-    if (line === "") return;
-    const gameNumber = parseInt(line.match(/\d+/)[0], 10);
-    console.log(gameNumber);
-    const winningNumbers = line
-        .match(/\:[\s\d]+\|/g)[0]
-        .match(/\d+/g)
-        .map((str) => parseInt(str, 10));
-    const myNumbers = line
-        .match(/\|.*$/g)[0]
-        .match(/\d+/g)
-        .map((str) => parseInt(str, 10));
-    let numOfMatches = 0;
-    myNumbers.forEach((myNumber) => {
-        winningNumbers.forEach((winningNumber) => {
-            if (myNumber === winningNumber) numOfMatches++;
+let lowestLocationNumber2 = 0;
+startRangePairs.forEach((pair) => {
+    const allMaps = [];
+    for (let i = 1; i <= 7; i++) {
+        allMaps.push(createMapper(parts[i]));
+    }
+    for (let i = 0; i < pair[1]; i++) {
+        let currentSeedTransformingToLocation = pair[0] + i;
+        allMaps.forEach((map) => {
+            currentSeedTransformingToLocation = map(
+                currentSeedTransformingToLocation,
+            );
         });
-    });
-    for (let i = 1; i <= cards[gameNumber - 1]; i++) {
-        for (let n = 0; n < numOfMatches; n++) {
-            cards[gameNumber + n]++;
+        if (
+            currentSeedTransformingToLocation < lowestLocationNumber2 ||
+            lowestLocationNumber2 === 0
+        ) {
+            lowestLocationNumber2 = currentSeedTransformingToLocation;
         }
     }
 });
-let numOfCards = 0;
-cards.forEach((cards) => (numOfCards += cards));
-console.log("cards: ", cards);
-console.log("numOfCards: ", numOfCards);
+console.log("lowestLocationNumber2: ", lowestLocationNumber2);
+
+function createMapper(part: string): (num: number) => number {
+    const lines = part.split("\n");
+    const maps: Map[] = [];
+    for (let i = 1; i < lines.length; i++) {
+        maps.push(new Map(lines[i]));
+    }
+    return (numberToMap: number): number => {
+        let outputNum = -1;
+        maps.forEach((map) => {
+            if (map.canMap(numberToMap)) {
+                outputNum = map.map(numberToMap);
+            }
+        });
+        return outputNum === -1 ? numberToMap : outputNum;
+    };
+}
